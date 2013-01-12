69e03f25089884cb23616099bad27b82ef5be55c
Taira <taira@taira-R500VJ.(none)>
2013-01-12 01:00:34 +0100

    correction sur le chargement des EJBs

--- tp-play/app/models/OPowerService.scala
+++ tp-play/app/models/OPowerService.scala
@@ -1,6 +1,7 @@
 package models
 
 import javax.naming.InitialContext
+import javax.naming.Context._
 import util.Commons._
 
 /**
@@ -10,13 +11,13 @@ object OPowerService {
 
   // TODO replace these values with your settings
   // Name and type of the bean
-  val SERVICE_NAME = "businessImpl"
-  type SERVICE_TYPE = fr.istic.m2infopro.taa.opowerInterface
+  val SERVICE_NAME = "businessImpl"     // @Stateless(mappedName = "businessImpl") de l'implementtation
+  type SERVICE_TYPE = business.BusinessInterface  // package et nom de l'interface
 
   // Return a reference to the OPower service
   def apply() = {
     import collection.JavaConverters._
-    import javax.naming.Context
+    import javax.naming
     val env = Map(
       Context.INITIAL_CONTEXT_FACTORY -> "org.ow2.easybeans.component.smartclient.spi.SmartContextFactory",
       Context.PROVIDER_URL -> "smart://localhost:2503"
